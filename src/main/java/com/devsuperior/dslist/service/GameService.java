480228e941ff1ad8d12e71f4d85af423a85a16ee
André_Lucas <134009910+22346-Andre@users.noreply.github.com>
2025-05-21 15:19:40 -0300

    search listId/games

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.service;
 
+import com.devsuperior.dslist.GameMinProjection;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
@@ -27,4 +28,12 @@ public class GameService {
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection>result = gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
 }
